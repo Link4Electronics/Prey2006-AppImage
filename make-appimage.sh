8729bf7b5530beee0ef59eb3885dcf86900cb469
Link4Electronics <link4electronics@gmail.com>
2026-04-05 18:19:14 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -7,8 +7,6 @@ export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export DESKTOP=DUMMY
-export MAIN_BIN=prey06
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
