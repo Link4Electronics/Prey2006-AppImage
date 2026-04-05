1d56af93075deb786124e0e3476b4c7acb8dc6e1
Link4Electronics <link4electronics@gmail.com>
2026-04-05 01:44:19 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -20,6 +20,12 @@ get-debloated-pkgs --add-common --prefer-nano
 #make-aur-package PACKAGENAME
 
 # If the application needs to be manually built that has to be done down here
+echo "Making nightly build of Rednukem..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/FriskTheFallenHuman/Prey2006"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone "$REPO" ./Prey2006
+echo "$VERSION" > ~/version
 
 mkdir -p ./AppDir/bin
 cd Prey2006/neo && mkdir build && cd build
